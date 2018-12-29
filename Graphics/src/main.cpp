a205265de71216766f90771e7bef5217caccfe0d
Pravesh Gaire <praveshgaire2018@outlook.com>
2018-12-29 20:39:33 +0545

    shaders parser from file added

--- Graphics/src/main.cpp
+++ Graphics/src/main.cpp
@@ -1,6 +1,46 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ParsedShaderSource {
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ParsedShaderSource ParseShader(const std::string& filepath)
+{
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	ShaderType  type = ShaderType::NONE;
+	std::ifstream stream(filepath);
+	std::string line;
+	std::stringstream ss[2];
+
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+	return { ss[0].str(), ss[1].str() };
+}
 
 static unsigned int CompileShader(const std::string& source, unsigned int type) 
 {
@@ -81,28 +121,13 @@ int main(void)
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 	glEnableVertexAttribArray(0);
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
-
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0, 0, 1.0);\n"
-		"}\n";
-
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
-	glUseProgram(shader);
+	ParsedShaderSource source = ParseShader("res/shaders/basic.shader");
+	std::cout << "Vertex Shader" << std::endl;
+	std::cout << source.VertexSource << std::endl;
+	std::cout << "Fragment Shader" << std::endl;
+	std::cout << source.FragmentSource << std::endl;
+	//unsigned int shader = createshader(vertexshader, fragmentshader);
+	//gluseprogram(shader); 
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
