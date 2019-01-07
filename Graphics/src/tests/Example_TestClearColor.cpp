26aaee06ffa3e4a5f20f870636afb3eefe8d7a01
Pravesh Gaire <praveshgaire2018@outlook.com>
2019-01-07 17:00:08 +0545

    ClearColorTest added

--- Graphics/src/tests/Example_TestClearColor.cpp
+++ Graphics/src/tests/Example_TestClearColor.cpp
@@ -1,16 +1,10 @@
+// Make sure to exclude Application.cpp from project before running this test to avoid multiple main function error 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
-
-#include "VertexBuffer.h"
-#include "IndexBuffer.h"
-#include "VertexArray.h"
-#include "Shader.h"
-#include "Renderer.h"
-#include "texture.h"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
@@ -35,7 +29,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "Tests", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -52,54 +46,15 @@ int main(void)
 
 	std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
 
-	//float positions[] = 
-	//{
-	//	-50.0f, -50.0f, 0.0f, 0.0f,
-	//	 50.0f, -50.0f, 1.0f, 0.0f,
-	//	 50.0f,  50.0f, 1.0f, 1.0f,
-	//	-50.0f,  50.0f, 0.0f, 1.0f
-	//};
-	//
-	//unsigned int indices[] =
-	//{
-	//	0, 1, 2,
-	//	2, 3, 0
-	//};
-	
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_BLEND);
-
-	//unsigned int vao;
-	//glGenVertexArrays(1, &vao);
-	//glBindVertexArray(vao);
-	//
-	//VertexArray va;
-	//VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-	//
-	//VertexBufferLayout layout;
-	//layout.Push<float>(2);
-	//layout.Push<float>(2);
-	//va.AddBuffer(vb, layout);
-	//
-	//IndexBuffer ib(indices, 6);
-	//
-	//glm::mat4 proj = glm::ortho(0.0f,960.0f,0.0f,540.0f, -1.0f, 1.0f);
-	//glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-	//
-	//Shader shader("res/shaders/basic.shader");
-	//shader.Bind();
-	//shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.2f, 1.0f);
-	//
-	//Texture texture("res/textures/cherno.png");
-	//texture.Bind();
-	//shader.SetUniform1i("u_Texture", 0);
 
 	Renderer renderer;
 
 	#if defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-		bool err = glewInit() != GLEW_OK;
+	bool err = glewInit() != GLEW_OK;
 	#else
-		bool err = false;
+	bool err = false;
 	#endif
 	if (err)
 	{
@@ -114,9 +69,6 @@ int main(void)
 
 	test::TestClearColor test;
 
-	//glm::vec3 translationB(200, 200, 0);
-	//glm::vec3 translationA(400, 200, 0);
-
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -127,30 +79,12 @@ int main(void)
 		renderer.Clear();
 
 		test.OnUpdate(0.0f);
-		test.OnRender();  
+		test.OnRender();
 
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-		//{
-		//	ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-		//	ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-		//	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / //ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		//}
-		//
-		//{
-		//	glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-		//	glm::mat4 mvp = proj * view * model;
-		//	shader.SetUniformMat4f("u_MVP", mvp);
-		//	renderer.Draw(va, ib, shader);
-		//}
-		//{
-		//	glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-		//	glm::mat4 mvp = proj * view * model;
-		//	shader.SetUniformMat4f("u_MVP", mvp);
-		//	renderer.Draw(va, ib, shader);
-		//}
-
+		
 		test.OnImGuiRender();
 
 		ImGui::Render();
