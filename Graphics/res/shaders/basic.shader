06adf0de04767a6e3e0b559470e5b26e3160281d
Pravesh Gaire <praveshgaire2018@outlook.com>
2019-02-15 18:20:28 +0545

    Added Camera and more cubes

--- Graphics/res/shaders/basic.shader
+++ Graphics/res/shaders/basic.shader
@@ -28,5 +28,6 @@ uniform sampler2D u_Texture;
 void main()
 {
 	vec4 texColor = texture(u_Texture, v_TexCoord);
+	
 	color = texColor;
 };
--- Graphics/src/main.cpp
+++ Graphics/src/main.cpp
@@ -6,156 +6,124 @@
 
 #include "Renderer.h"
 #include "texture.h"
+#include "camera.h"
 
 #include <iostream>
 
-float x = 1.0f, y = 1.0f, z = 1.0f, angle = 0.0f, cam_depth = -300.0f;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	angle = 0.0005f;
-	z = 0.0f;
-	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
-	{
-		y = -1.0f;
-		x = 0.0f;
-		//angle-=0.001f;
-	//	std::cout << angle << std::endl;
-	}
-	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
-	{
-		y = 1.0f;
-		x = 0.0f;
-		//angle+=0.001f;
-	//	std::cout << angle << std::endl;
-	}
-	if (key == GLFW_KEY_UP && action == GLFW_PRESS)
-	{
-		x = -1.0f;
-		y = 0.0f;
-		//angle-=0.001f;
-	//	std::cout << angle << std::endl;
-	}
-	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
-	{
-		x = 1.0f;
-		y = 0.0f;
-		//angle+=0.001f;
-	//	std::cout << angle << std::endl;
-	}
-	if (key == GLFW_KEY_1 && action == GLFW_PRESS)
-	{
-		cam_depth+=10;
-		//angle+=0.001f;
-		//std::cout << cam_depth << std::endl;
-	}
-	if (key == GLFW_KEY_0 && action == GLFW_PRESS)
-	{
-		cam_depth-=10;
-		//angle+=0.001f;
-		//std::cout << cam_depth << std::endl;
-	}
-}
+// Settings
+const unsigned int screen_width = 800;
+const unsigned int screen_height = 600;
 
-glm::mat4 glPerspective(const float angle, const float ar, const float zNear, const float zFar)
-{
-	const float zRange = zFar-zNear;
-	const float tanHalfFOV = tan(angle / 2.0);
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = screen_width / 2.0f;
+float lastY = screen_height / 2.0f;
+bool firstMouse = true;
 
-	glm::mat4 m(0.0f);
+// Timing
+float currentFrame = 0.0f;
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
-	m[0][0] = 1.0f / (tanHalfFOV * ar);
-	m[1][1] = 1.0f / tanHalfFOV;
-	m[2][2] = zFar / zRange;
-	m[2][3] = 1.0f;
-	m[3][2] = -(zFar * zNear) / zRange;
+int main() {
 
-	return m;
-}
-
-
-int main(void) 
-{
-	if (!glfwInit())
-		return -1;
-
+	// glfw: Initialize and configuration
+	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	GLFWwindow* window;
-	window = glfwCreateWindow(960,540, "Computer Graphics", NULL, NULL);
-	if (!window)
+	// glfw window creation
+	// --------------------
+	GLFWwindow* window = glfwCreateWindow(screen_width, screen_height, "Graphics Project", NULL, NULL);
+	if (window == NULL)
 	{
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
+	}
 
-	int screenWidth, screenHeight;
-	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
-	glViewport(0, 0, screenWidth, screenHeight);
-
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
+	// configure global opengl state
 	glEnable(GL_DEPTH_TEST);
-		
-	std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
-	
-	std::cout << "Use\n0 to zoom out\n1 to zoom in\narrows to rotate about x and y axis" << std::endl;
 
-	const char* glsl_version = "#version 330 core";
+	// build and compile shaders
+	Shader shader("res/shaders/basic.shader");
+	shader.Bind();
 
-	glfwSetKeyCallback(window, key_callback);
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
-	GLfloat positions[] = {
-		-0.5f * 200, -0.5f * 200, -0.5f * 200,  0.0f, 0.0f,
-		0.5f * 200, -0.5f * 200, -0.5f * 200,  1.0f, 0.0f,
-		0.5f * 200,  0.5f * 200, -0.5f * 200,  1.0f, 1.0f,
-		0.5f * 200,  0.5f * 200, -0.5f * 200,  1.0f, 1.0f,
-		-0.5f * 200,  0.5f * 200, -0.5f * 200,  0.0f, 1.0f,
-		-0.5f * 200, -0.5f * 200, -0.5f * 200,  0.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
 
-		-0.5f * 200, -0.5f * 200,  0.5f * 200,  0.0f, 0.0f,
-		0.5f * 200, -0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
-		0.5f * 200,  0.5f * 200,  0.5f * 200,  1.0f, 1.0f,
-		0.5f * 200,  0.5f * 200,  0.5f * 200,  1.0f, 1.0f,
-		-0.5f * 200,  0.5f * 200,  0.5f * 200,  0.0f, 1.0f,
-		-0.5f * 200, -0.5f * 200,  0.5f * 200,  0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
 
-		-0.5f * 200,  0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
-		-0.5f * 200,  0.5f * 200, -0.5f * 200,  1.0f, 1.0f,
-		-0.5f * 200, -0.5f * 200, -0.5f * 200,  0.0f, 1.0f,
-		-0.5f * 200, -0.5f * 200, -0.5f * 200,  0.0f, 1.0f,
-		-0.5f * 200, -0.5f * 200,  0.5f * 200,  0.0f, 0.0f,
-		-0.5f * 200,  0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
 
-		0.5f * 200,  0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
-		0.5f * 200,  0.5f * 200, -0.5f * 200,  1.0f, 1.0f,
-		0.5f * 200, -0.5f * 200, -0.5f * 200,  0.0f, 1.0f,
-		0.5f * 200, -0.5f * 200, -0.5f * 200,  0.0f, 1.0f,
-		0.5f * 200, -0.5f * 200,  0.5f * 200,  0.0f, 0.0f,
-		0.5f * 200,  0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
 
-		-0.5f * 200, -0.5f * 200, -0.5f * 200,  0.0f, 1.0f,
-		0.5f * 200, -0.5f * 200, -0.5f * 200,  1.0f, 1.0f,
-		0.5f * 200, -0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
-		0.5f * 200, -0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
-		-0.5f * 200, -0.5f * 200,  0.5f * 200,  0.0f, 0.0f,
-		-0.5f * 200, -0.5f * 200, -0.5f * 200,  0.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f
+	};
 
-		-0.5f * 200,  0.5f * 200, -0.5f * 200,  0.0f, 1.0f,
-		0.5f * 200,  0.5f * 200, -0.5f * 200,  1.0f, 1.0f,
-		0.5f * 200,  0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
-		0.5f * 200,  0.5f * 200,  0.5f * 200,  1.0f, 0.0f,
-		-0.5f * 200,  0.5f * 200,  0.5f * 200,  0.0f, 0.0f,
-		-0.5f * 200,  0.5f * 200, -0.5f * 200,  0.0f, 1.0f
+	// world space positions of our cubes
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
 	unsigned int vao;
@@ -163,16 +131,13 @@ int main(void)
 	glBindVertexArray(vao);
 
 	VertexArray va;
-	VertexBuffer vb(positions, 6 * 5 * 6 * sizeof(float));
+	VertexBuffer vb(vertices, 6 * 5 * 6 * sizeof(float));
 
 	VertexBufferLayout layout;
 	layout.Push<float>(3);
 	layout.Push<float>(2);
 
 	va.AddBuffer(vb, layout);
-
-	Shader shader("res/shaders/basic.shader");
-	shader.Bind();
 
 	Texture texture("res/textures/image.jpg");
 	texture.Bind();
@@ -181,35 +146,92 @@ int main(void)
 
 	Renderer renderer;
 
-	glm::mat4 projection, view, model;
+	while (!glfwWindowShouldClose(window)) {
+		
+		// per frame logic
+		currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 
-	projection = glPerspective(45.0f, (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 1000.0f);
-	projection = glm::perspective(45.0f, (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 1000.0f);
-	view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -100.0f));
-	model = glm::scale(glm::mat4(1.0f),glm::vec3(0.2f, 0.2f, 0.2f));
+		// input
+		processInput(window);
 
-	shader.SetUniformMat4f("u_Proj", projection);
-	shader.SetUniformMat4f("u_View", view);
-	shader.SetUniformMat4f("u_Model", model);
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-
+		// render
 		renderer.Clear();
 
-		model = glm::rotate(model, angle, glm::vec3(x, y, z));
-		shader.SetUniformMat4f("u_Model", model);
+		// projection matrix
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)screen_width / (float)screen_height, 0.1f, 100.0f);
+		shader.SetUniformMat4f("u_Proj", projection);
 
-		view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, cam_depth));
+		// camera / view transformation
+		glm::mat4 view = camera.GetViewMatrix();
 		shader.SetUniformMat4f("u_View", view);
 
-		renderer.Draw(va, shader);
+		// render boxes
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			shader.SetUniformMat4f("u_Model", model);
+			renderer.Draw(va, shader);
+		}
+
+		// swap buffers and poll IO
 		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
-
+	
 	return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
 }
